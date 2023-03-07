28f83fd42b6aa4e4dca5aa9d828603adad3000d7
Havish Soowamber <soowamhc@gmail.com>
2023-03-07 11:03:27 +0400

    Added all models and foreign keys

---
+++ db/migrate/20230307065434_create_plants.rb
@@ -0,0 +1,12 @@
+class CreatePlants < ActiveRecord::Migration[7.0]
+  def change
+    create_table :plants do |t|
+      t.string :name
+      t.text :description
+      t.string :scientific_name
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,57 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_064327) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_070000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "conditions", force: :cascade do |t|
+    t.string "sun"
+    t.string "water"
+    t.string "climate"
+    t.bigint "plant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["plant_id"], name: "index_conditions_on_plant_id"
+  end
+
+  create_table "gardens", force: :cascade do |t|
+    t.string "name"
+    t.bigint "plant_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["plant_id"], name: "index_gardens_on_plant_id"
+    t.index ["user_id"], name: "index_gardens_on_user_id"
+  end
+
+  create_table "plants", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "scientific_name"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_plants_on_category_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.text "comment"
+    t.boolean "done"
+    t.bigint "plant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["plant_id"], name: "index_tasks_on_plant_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +70,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_064327) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "conditions", "plants"
+  add_foreign_key "gardens", "plants"
+  add_foreign_key "gardens", "users"
+  add_foreign_key "plants", "categories"
+  add_foreign_key "tasks", "plants"
 end

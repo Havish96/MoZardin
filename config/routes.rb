1427cb8b10985e41a6ad94eea87f43a8c08cab83
manuela dongmo <manueladongmo2@gmail.com>
2023-03-09 11:22:30 +0400

    FirstStep

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "plants#home"
+  get '/gardens/plants', to: 'gardens#show', as: 'plant'
 
   resources :plants
-  resources :gardens
+  resources :gardens, only: %i[create show]
 end

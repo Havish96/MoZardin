116371129feedc9bd0eeb86d460a54355c3ed2b8
Jeremy Chan Yue Nang <yiungnye@gmail.com>
2023-03-09 07:31:19 +0400

    added guide to routes and initialized guides controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'guides/index'
+  get 'guide/index'
   devise_for :users
   root to: "plants#home"
 
   resources :plants
   resources :gardens
+  resources :guides, only: %i[index show]
 end

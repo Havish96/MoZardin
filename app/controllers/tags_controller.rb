c9abeb4a647a219b25b08bcdfec5560e24ca8714
ezekielramtano <codingwithezekiel@gmail.com>
2023-03-10 12:54:03 +0400

    completed tag modals

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,20 +1,16 @@
 class TagsController < ApplicationController
   def create
-    @user = current_user
     @tag = Tag.new(params_tag)
-    @tag.user = @user
-    @plant = Plant.find(params[:plant_id])
-    @tag.plant = @plant
-    if @plant.save
-      redirect_to tags_path
+    if @tag.save
+      redirect_to garden_path(params_tag[:garden_id])
     else
-      redirect_to plant_path(@plant), status: :unprocessable_entity
+      redirect_to plant_path(params_tag[:plant_id]), status: :unprocessable_entity
     end
   end
 
   private
 
   def params_tag
-    params.require(:tag).permit(:age, :garden_id, :location)
+    params.require(:tag).permit(:age, :garden_id, :location, :plant_id)
   end
 end

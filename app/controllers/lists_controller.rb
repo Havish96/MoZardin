16dc0e64d1ec2062e59cc2445883eaab4c6d4530
Jeremy Chan Yue Nang <yiungnye@gmail.com>
2023-03-14 13:08:50 +0400

    implemented progress bar

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,6 @@
 class ListsController < ApplicationController
   def index
-    @lists = List.all.where(user: current_user)
+    @list = List.find_by(user: current_user)
     @tasks = Task.where(list: @list)
     @task = Task.new
   end

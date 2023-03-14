07c18523edf1c8e285cc229895cec0dd95d23ad7
ezekielramtano <codingwithezekiel@gmail.com>
2023-03-14 15:13:19 +0400

    working both on host an terminal

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :photo
+
+  after_save :create_default_garden
+
+  private
+
+  def create_default_garden
+    garden = Garden.new(name: "Default Garden")
+    garden.user = self
+    garden.save
+  end
 end

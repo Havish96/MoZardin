47125d841e1b9246b693aa0370616a3b840b9f74
Havish Soowamber <soowamhc@gmail.com>
2023-03-14 16:19:01 +0400

    added city for login and sign up page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,12 @@ class TasksController < ApplicationController
     end
   end
 
+  def weather_today
+    url = "http://api.weatherapi.com/v1/current.json?key=bb8ae13d2adc43af89651804231303&q=#{current_user.city},Mauritius"
+    user_serialized = URI.open(url).read
+    JSON.parse(user_serialized)
+  end
+
   private
 
   def task_params

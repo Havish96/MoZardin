0568f75bafe0c1c924d6de916b6c39a92d2260d9
Jeremy Chan Yue Nang <yiungnye@gmail.com>
2023-03-14 15:11:19 +0400

    progress on tasks page

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,6 @@
 class ListsController < ApplicationController
   def index
-    @list = List.find_by(user: current_user)
+    @list = List.all.where(user: current_user)
     @tasks = Task.where(list: @list)
     @task = Task.new
   end

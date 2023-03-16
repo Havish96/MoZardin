62a50a5ef63b563c8620933a7ebd8ad489c9695e
Jeremy Chan Yue Nang <yiungnye@gmail.com>
2023-03-16 11:16:24 +0400

    added region to card

--- app/javascript/controllers/weather_controller.js
+++ app/javascript/controllers/weather_controller.js
@@ -2,9 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="weather"
 export default class extends Controller {
-  static targets = ["temperature", "address"]
+  static targets = ["temperature", "address", "region"]
   connect() {
-    fetch('https://api.weatherapi.com/v1/forecast.json?key=bb8ae13d2adc43af89651804231303&q=${this.addressTarget.textContent}-mauritius', {
+    fetch(`https://api.weatherapi.com/v1/forecast.json?key=bb8ae13d2adc43af89651804231303&q=${this.addressTarget.textContent}-mauritius`, {
       method: "GET",
       headers: { "Accept": "application/json" }
     })
@@ -21,7 +21,7 @@ export default class extends Controller {
   #updateGarden(data) {
     console.log(data)
     this.temperatureTarget.innerText = `${Math.round(data.current.temp_c)}`
-    // this.addressTarget.innerText = data.location.region
+    this.regionTarget.innerText = data.location.region
     // const today = new Date();
     // const localOffset = data.timezone + today.getTimezoneOffset() * 60
     // const localDate = new Date(today.setUTCSeconds(localOffset))

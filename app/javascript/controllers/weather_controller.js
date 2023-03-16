cd2a241cff159c50176c50ddc171e7d1a2f36b37
Jeremy Chan Yue Nang <yiungnye@gmail.com>
2023-03-16 11:52:40 +0400

    made api key secure in .env

--- app/javascript/controllers/weather_controller.js
+++ app/javascript/controllers/weather_controller.js
@@ -2,9 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="weather"
 export default class extends Controller {
+  static values = {
+    apiKey: String
+  }
   static targets = ["temperature", "address", "region"]
   connect() {
-    fetch(`https://api.weatherapi.com/v1/forecast.json?key=bb8ae13d2adc43af89651804231303&q=${this.addressTarget.textContent}-mauritius`, {
+    console.log(this.apiKeyValue)
+    fetch(`https://api.weatherapi.com/v1/forecast.json?key=${this.apiKeyValue}&q=${this.addressTarget.textContent}-mauritius`, {
       method: "GET",
       headers: { "Accept": "application/json" }
     })
@@ -14,9 +18,9 @@ export default class extends Controller {
   }
 
 
-  initialize() {
-    this.apiKey = "bb8ae13d2adc43af89651804231303"
-  }
+  // initialize() {
+  //   this.apiKey = this.keyValue
+  // }
 
   #updateGarden(data) {
     console.log(data)

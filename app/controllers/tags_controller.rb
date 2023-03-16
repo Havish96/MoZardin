7bc003c07a23c91a8742012cafd2498b227cd98a
Havish Soowamber <soowamhc@gmail.com>
2023-03-16 14:55:45 +0400

    added generate_tasks when adding a new plant to garden

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,6 +2,7 @@ class TagsController < ApplicationController
   def create
     @tag = Tag.new(params_tag)
     if @tag.save
+      TasksController.generate_tasks(current_user)
       redirect_to garden_path(params_tag[:garden_id])
     else
       redirect_to plant_path(params_tag[:plant_id]), status: :unprocessable_entity
